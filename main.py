a16bde00b6b7a80fb14f776c6224175fe4e0919c
jeovanecatagua <jeovane.mendes@catagua.com.br>
2025-01-29 14:59:22 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,9 +3,29 @@ import Pages.reserva_sala_reuniao.Create as PagesCreateReserva
 import Pages.reserva_sala_reuniao.List as PagesListReserva
 import Pages.reserva_sala_reuniao.validacao_user as acesso
 from Controllers.ControllersReservas import create_tb, obter_perfil_usuario
+import os
 
 create_tb()
 print("Tabelas criadas com sucesso!")
+
+# Caminho do banco de dados
+db_path = "dados_projeto.db"
+
+# Criar botão de download
+with open(db_path, "rb") as f:
+    st.download_button(
+        label="Baixar Banco de Dados",
+        data=f,
+        file_name="dados_projeto.db",
+        mime="application/x-sqlite3",
+    )
+
+# Verifica se o arquivo do banco existe
+if os.path.exists(db_path):
+    st.write("Banco de dados encontrado!")
+else:
+    st.write("⚠️ O banco de dados não foi encontrado no diretório do aplicativo!")
+
 
 # --- Define layout baseado no login ---
 if "authenticated" in st.session_state and st.session_state["authenticated"]:
